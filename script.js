9fa6c6659f8eab6ab3700752bb7d97f52555901c
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-07 19:55:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function firstChar(text) {
 
 
 	let a=text.trim();
-	return a[0];
+	return a.charAt(0);
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-const text=prompt("Enter :")
-alert(firstChar(text));
+ const text=prompt("Enter :")
+alert(firstChar(text)); 
   
